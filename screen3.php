e379236cbaf1ef41962eed7164d5cc353a9549d2
Nathan Fishel <nfishel@emich.edu>
2020-10-21 22:57:42 -0400

    testing database query

--- screen3.php
+++ screen3.php
@@ -32,10 +32,39 @@
 			</td>
 		</tr>	
 		<tr>
-		<td style="width: 350px" colspan="3" align="center">
-			<div id="bookdetails" style="overflow:scroll;height:180px;width:400px;border:1px solid black;background-color:LightBlue">
-			<table>
-			<tr><td align='left'><button name='btnCart' id='btnCart' onClick='cart("123441", "", "Array", "all")'>Add to Cart</button></td><td rowspan='2' align='left'>iuhdf</br>By Avi Silberschatz</br><b>Publisher:</b> McGraw-Hill,</br><b>ISBN:</b> 123441</t> <b>Price:</b> 12.99</td></tr><tr><td align='left'><button name='review' id='review' onClick='review("123441", "iuhdf")'>Reviews</button></td></tr><tr><td colspan='2'><p>_______________________________________________</p></td></tr><tr><td align='left'><button name='btnCart' id='btnCart' onClick='cart("978-0316055437", "", "Array", "all")'>Add to Cart</button></td><td rowspan='2' align='left'>title</br>By fname lname</br><b>Publisher:</b> pub,</br><b>ISBN:</b> 978-0316055437</t> <b>Price:</b> 12.99</td></tr><tr><td align='left'><button name='review' id='review' onClick='review("978-0316055437", "title")'>Reviews</button></td></tr><tr><td colspan='2'><p>_______________________________________________</p></td></tr><tr><td align='left'><button name='btnCart' id='btnCart' onClick='cart("978-0345339706", "", "Array", "all")'>Add to Cart</button></td><td rowspan='2' align='left'>Lord of the Rings, The Fellowship of the</br>By J.R.R. Tolkien</br><b>Publisher:</b> Del Rey,</br><b>ISBN:</b> 978-0345339706</t> <b>Price:</b> 8.09</td></tr><tr><td align='left'><button name='review' id='review' onClick='review("978-0345339706", "Lord of the Rings, The Fellowship of the")'>Reviews</button></td></tr><tr><td colspan='2'><p>_______________________________________________</p></td></tr><tr><td align='left'><button name='btnCart' id='btnCart' onClick='cart("978-0590353427", "", "Array", "all")'>Add to Cart</button></td><td rowspan='2' align='left'>Harry Potter and the Sorcerer Stone</br>By J.K. Rowling</br><b>Publisher:</b> Scholastic,</br><b>ISBN:</b> 978-0590353427</t> <b>Price:</b> 8.47</td></tr><tr><td align='left'><button name='review' id='review' onClick='review("978-0590353427", "Harry Potter and the Sorcerer Stone")'>Reviews</button></td></tr><tr><td colspan='2'><p>_______________________________________________</p></td></tr>			</table>
+		<?
+			$query = 'Select * From Review';
+			$db_conn = pg_connect("host=ec2-3-218-75-21.compute-1.amazonaws.com dbname=d8p0qs8v3fbf9m user=gymsvpkhkckshh password=68db7ff943798b07abc442d46449c9d2f4bfcd38be0f79023a630bf67b3b3a8a");
+
+			$result =  pg_query ($connection, $query);
+			if (!$result) {
+				echo "<h3>An error occurred.</h3>";
+			}
+			echo "<h3>$result</h3";
+
+			pg_close($db_conn);
+		?>
+			<td style='width: 350px' colspan='3' align='center'>
+				<div id="bookdetails" style="overflow:scroll;height:180px;width:400px;border:1px solid black;background-color:LightBlue">
+				<table>
+				<tr>
+					<td align='left'>
+						<button name='btnCart' id='btnCart' onClick='cart("123441", "", "Array", "all")'>Add to Cart</button>
+					</td>
+					<td rowspan='2' align='left'>iuhdf</br>By Avi Silberschatz</br><b>Publisher:</b> McGraw-Hill,</br><b>ISBN:</b> 123441</t> <b>Price:</b> 12.99</td>
+				</tr>
+				<tr>
+					<td align='left'>
+						<button name='review' id='review' onClick='review("123441", "iuhdf")'>Reviews</button>
+					</td>
+				</tr>
+				<tr>
+					<td colspan='2'>
+						<p>_______________________________________________</p>
+					</td>
+			</td>
+		
+	</table>
 			</div>
 			
 			</td>
@@ -58,16 +87,5 @@
 			</td>
 		</tr>
 	</table>
-	<?
-$db_handle = pg_connect("host=ec2-3-218-75-21.compute-1.amazonaws.com dbname=d8p0qs8v3fbf9m user=gymsvpkhkckshh password=68db7ff943798b07abc442d46449c9d2f4bfcd38be0f79023a630bf67b3b3a8a");
-if($db_handle){
-	echo 'Connection successful';
-} else {
-
-	echo 'Connection attempt failed.';
-	
-	}
-pg_close($db_handle);
-?>
 </body>
 </html>
