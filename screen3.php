73f08d49873ec6b7b20366f1ad7f2e49f98c0d66
Nathan Fishel <nfishel@emich.edu>
2020-10-21 23:04:46 -0400

    testing database Query

--- screen3.php
+++ screen3.php
@@ -36,7 +36,7 @@
 			$query = 'Select * From Review;';
 			$db_conn = pg_connect("host=ec2-3-218-75-21.compute-1.amazonaws.com dbname=d8p0qs8v3fbf9m user=gymsvpkhkckshh password=68db7ff943798b07abc442d46449c9d2f4bfcd38be0f79023a630bf67b3b3a8a");
 
-			$result =  pg_query ($connection, $query);
+			$result =  pg_query($connection, $query);
 			if (!$result) {
 				echo "<h3>An error occurred.</h3>";
 			}
