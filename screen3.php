7cc6650780e606ad60f1c4b101babe0691c6ad7f
Nathan Fishel <nfishel@emich.edu>
2020-11-05 19:27:47 -0500

    Testing search form

--- screen3.php
+++ screen3.php
@@ -1,6 +1,13 @@
 
 <!-- Figure 3: Search Result Screen by Prithviraj Narahari, php coding: Alexander Martens -->
 <html>
+<?
+		// if (!empty($_POST)) {
+			$db = pg_connect("host=ec2-3-218-75-21.compute-1.amazonaws.com dbname=d8p0qs8v3fbf9m user=gymsvpkhkckshh password=68db7ff943798b07abc442d46449c9d2f4bfcd38be0f79023a630bf67b3b3a8a");
+			echo $_POST;
+			pg_close($db);
+		// }
+	?>
 <head>
 	<title> Search Result - 3-B.com </title>
 	<script>
