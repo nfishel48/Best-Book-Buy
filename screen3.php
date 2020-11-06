6e266f75f97035b4d8f5cea85de6979b412ba450
Nathan Fishel <nfishel@emich.edu>
2020-11-05 20:43:42 -0500

    Testing search form

--- screen3.php
+++ screen3.php
@@ -3,7 +3,7 @@
 <html>
 <?
 			$db = pg_connect("host=ec2-3-218-75-21.compute-1.amazonaws.com dbname=d8p0qs8v3fbf9m user=gymsvpkhkckshh password=68db7ff943798b07abc442d46449c9d2f4bfcd38be0f79023a630bf67b3b3a8a");
-			$query = "Select * From Book Where "+$_POST['searchon']+" LIKE %"+$_POST['searchfor']+"%;";
+			$query = "Select * From Book Where ".$_POST['searchon']." LIKE %".$_POST['searchfor']."%;";
 			echo $query;
 			$result =  pg_query($db_conn, $query);
 			echo $result;
