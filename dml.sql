1804f093b2edb4522496aebcc2d1e5df11252b12
Nathan Fishel <nfishel@emich.edu>
2020-11-06 18:50:43 -0500

    Testing DB pull

--- dml.sql
+++ dml.sql
@@ -1,6 +1,9 @@
 INSERT INTO Book(ISBN, Title, Author, Publisher, Price)
 VALUES ('10247184712349', '1984', 'George Orwell', 'Penguin', '19.99');
 
+INSERT INTO Book(ISBN, Title, Author, Publisher, Price)
+VALUES ('10247184712342', 'Animal Farm', 'George Orwell', 'Penguin', '20.00');
+
 INSERT INTO Customer( Username, First_Name, Last_Name, PIN, Address, City, State, Zip, CCtype, CCnum, ExpDate)
 VALUES ('Test', 'Tom', 'Tester', '123', '223 street', 'Ypsilanti','Michigan','48197','VISA','49483020483','10/23'); 
 
--- screen3.php
+++ screen3.php
@@ -39,7 +39,7 @@
 				</form>
 			</td>
 		</tr>
-			
+		<tr>	
 		<?
 					$db = pg_connect("host=ec2-3-218-75-21.compute-1.amazonaws.com dbname=d8p0qs8v3fbf9m user=gymsvpkhkckshh password=68db7ff943798b07abc442d46449c9d2f4bfcd38be0f79023a630bf67b3b3a8a");
 					$query = "Select * From Book Where ".$_POST['searchon']." LIKE '%".$_POST['searchfor']."%';";
@@ -55,7 +55,7 @@
 					
 					pg_close($db);
 		
-		echo '<tr>';
+		//echo '<tr>';
 		echo	"<td style='width: 350px' colspan='3' align='center'>";
 			echo '<div id="bookdetails" style="overflow:scroll;height:180px;width:400px;border:1px solid black;background-color:LightBlue">';
 				echo '<table>';
@@ -76,9 +76,10 @@
 						echo "<p>_______________________________________________</p>";
 					echo "</td>";
 			echo "</td>";
-		echo "</tr>";
+		//echo "</tr>";
 		}
 		?>
+		</tr>
 	</table>
 			</div>
 			
