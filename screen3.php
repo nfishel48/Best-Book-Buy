6eb8d692b8a3f86e8eec263bb12d89452987b5a1
Nathan Fishel <nfishel@emich.edu>
2020-11-07 14:06:52 -0500

    Testing add to cart

--- screen3.php
+++ screen3.php
@@ -45,12 +45,10 @@
 			<div id="bookdetails" style="overflow:scroll;height:200px;width:800px;border:1px solid black;background-color:LightBlue">
 				<table>
 		<?
-		//Trigger fuction if button is pressed
 		if(array_key_exists('button1', $_POST)) { 
 			echo "button pressed   ";
 			addToCart($ISBN); 
 			} 
-		// if button is pressed add to cart
 		$db = pg_connect("host=ec2-3-218-75-21.compute-1.amazonaws.com dbname=d8p0qs8v3fbf9m user=gymsvpkhkckshh password=68db7ff943798b07abc442d46449c9d2f4bfcd38be0f79023a630bf67b3b3a8a");
 		if($_POST['searchon'] == '*'){
 			$query = "Select * from Book;";
