9296f17dc36bb4f44675e9baab14fe7656e1bb0c
Nathan Fishel <nfishel@emich.edu>
2020-11-07 14:01:08 -0500

    Testing add to cart

--- screen3.php
+++ screen3.php
@@ -45,6 +45,11 @@
 			<div id="bookdetails" style="overflow:scroll;height:200px;width:800px;border:1px solid black;background-color:LightBlue">
 				<table>
 		<?
+		//Trigger fuction if button is pressed
+		if(array_key_exists('button1', $_POST)) { 
+			echo "button pressed   ";
+			addToCart($ISBN); 
+		} 
 		// if button is pressed add to cart
 		$db = pg_connect("host=ec2-3-218-75-21.compute-1.amazonaws.com dbname=d8p0qs8v3fbf9m user=gymsvpkhkckshh password=68db7ff943798b07abc442d46449c9d2f4bfcd38be0f79023a630bf67b3b3a8a");
 		if($_POST['searchon'] == '*'){
@@ -62,12 +67,6 @@
 			$Price = $row[4];
 			$book = "<td rowspan='2' align='left'>".$Title."</br>".$Author."</br><b>Publisher:</b>".$Publisher.",</br><b>ISBN:</b>".$ISBN."</t> <b>Price:</b>".$Price."</td>";
 			$review = "<input name='review' id='review' type='submit' value='".$ISBN."' onClick='review(".$ISBN.", ".$Title.")'></input>";
-			//Trigger fuction if button is pressed
-			if(array_key_exists('button1', $_POST)) { 
-				echo "button pressed   ";
-				addToCart($ISBN); 
-			} 
-
 					echo '<tr>';
 						echo "<td align='left'>";
 							echo '<form method="post" action="shopping_cart.php">'; 
