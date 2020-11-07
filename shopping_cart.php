3c09da79c0c1ba667028642dc28e733a42ca6205
Nick <nick.gilliam2@gmail.com>
2020-11-06 20:20:50 -0500

    added debugging

--- shopping_cart.php
+++ shopping_cart.php
@@ -22,8 +22,9 @@
 				$remove_book = pg_query($db, "delete from order_book where order_number = (select number from \"order\" where placed = false) and book_isbn = '$isbn'");
 			}
 		} else if (!empty($_POST)){
-			echo($_POST);
-			print($_POST);
+			for ($input in $_POST){
+				print($input);
+			}
 		}
 		
 		pg_close($db);
