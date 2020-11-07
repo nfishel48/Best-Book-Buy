d19ce482b435e50f18568d47b7df6e87393efa72
Nathan Fishel <nfishel@emich.edu>
2020-11-06 22:24:26 -0500

    Testing DB pull

--- screen4.php
+++ screen4.php
@@ -12,31 +12,22 @@
 }
 </style>
 </head>
-<body>
-	<tr>
-		<td align="center">
-			<h5> Reviews For:</h5>
-		</td>
-		<td align="left">
-			<h5> </h5>
-		</td>
-	</tr>
+	<table align="center" style="border:1px solid blue;">
+		<tr>
+			<td align="center">';
+				</h5>
+			</td>
+			<td align="left">
+				<h5> </h5>
+			</td>
+		</tr>
 <?
 	$query = "Select * From Review Where book ='".$_POST['review']."';";
 	$db_conn = pg_connect("host=ec2-3-218-75-21.compute-1.amazonaws.com dbname=d8p0qs8v3fbf9m user=gymsvpkhkckshh password=68db7ff943798b07abc442d46449c9d2f4bfcd38be0f79023a630bf67b3b3a8a");
 	$result =  pg_query($db_conn, $query);
 	while ($row = pg_fetch_row($result)) {
 		$a = "<tr><td><h3>Username: ".$row[1]."</h3></td></tr><tr><td><p>".$row[3]."</p></td></tr><hr>"; 	
-	echo '<table align="center" style="border:1px solid blue;">';
-		echo '<tr>';
-			echo '<td align="center">';
-				echo '<h5> Reviews For:</h5>';
-			echo '</td>';
-			echo '<td align="left">';
-				echo '<h5> </h5>';
-			echo '</td>';
-		echo '</tr>';
-			
+
 		echo '<tr>';
 			echo '<td colspan="2">';
 			echo '<div id="bookdetails" style="overflow:scroll;height:200px;width:300px;border:1px solid black;">';
