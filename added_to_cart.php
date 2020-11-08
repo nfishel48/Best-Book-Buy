18604777f7c18d72cc0c707f2a4a3278f126fd43
Nathan Fishel <nfishel@emich.edu>
2020-11-08 17:01:58 -0500

    added sql query to added to cart

--- added_to_cart.php
+++ added_to_cart.php
@@ -1,3 +1,6 @@
 <?
-echo 'This button worked'
+echo 'This button worked';
+$add = "insert into order_book (order_number, book_isbn, quantity) values(1, ".$_POST['cart'].", 1);";
+echo $add;
+pg_query($db, $add);
 ?>
