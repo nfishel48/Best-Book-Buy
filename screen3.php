95ccb388e4b2a0c46da63853878162761bf3d0af
Nathan Fishel <nfishel@emich.edu>
2020-11-08 14:28:56 -0500

    button working now

--- screen3.php
+++ screen3.php
@@ -46,7 +46,7 @@
 				<table>
 		<?
 		if(array_key_exists('button1', $_POST)) { 			//call function
-			addToCart($ISBN); 
+			addToCart(); 
 			} 
 		$db = pg_connect("host=ec2-3-218-75-21.compute-1.amazonaws.com dbname=d8p0qs8v3fbf9m user=gymsvpkhkckshh password=68db7ff943798b07abc442d46449c9d2f4bfcd38be0f79023a630bf67b3b3a8a");
 		if($_POST['searchon'] == '*'){
@@ -87,12 +87,13 @@
 					echo "</td>";
 			echo "</td>";
 		//echo "</tr>";
-		}
-		function addToCart($ISBN){
+		function addToCart(){
 			$add = "insert into order_book (order_number, book_isbn, quantity) values(1, ".$ISBN.", 1);";
 			echo $add;
 			pg_query($db, $add);
 		}
+		}
+
 		pg_close($db);
 		?>
 	</table>
