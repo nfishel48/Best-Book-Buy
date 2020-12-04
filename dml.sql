4a6899ca825167560b9d47f5dac013e2875e460f
Nathan Fishel <n_fishel-v@dcu.org>
2020-12-04 01:27:20 -0500

    fix table

--- dml.sql
+++ dml.sql
@@ -1,11 +1,8 @@
 INSERT INTO Book(ISBN, Title, Author, Publisher, Price)
-VALUES ('10247184712349', '1984', 'George Orwell', 'Penguin', '19.99');
+VALUES ('10247184712349', '1984', 'George Orwell', 'Penguin', '19');
 
 INSERT INTO Book(ISBN, Title, Author, Publisher, Price)   
-VALUES ('10247184712342', 'Animal Farm', 'George Orwell', 'Penguin', '20.00');
-
-INSERT INTO Book(ISBN, Title, Author, Publisher, Price)
-VALUES ('10247184712342', 'Animal Farm', 'George Orwell', 'Penguin', '20.00');
+VALUES ('10247184712342', 'Animal Farm', 'George Orwell', 'Penguin', '20');
 
 INSERT INTO Customer( Username, First_Name, Last_Name, PIN, Address, City, State, Zip, CCtype, CCnum, ExpDate)
 VALUES ('Test', 'Tom', 'Tester', '123', '223 street', 'Ypsilanti','Michigan','48197','VISA','49483020483','10/23'); 
@@ -18,3 +15,6 @@ VALUES ('nfishel', '10247184712349', 'Hated it don’t waste your time.');
 
 INSERT INTO Review(Author, Book, Content)
 VALUES ('Test', '10247184712349', 'Amazing book, must read for anyone.');
+
+INSERT INTO Order_t(number, Customer_id, placed)
+VALUES('1', '1', 'f');
