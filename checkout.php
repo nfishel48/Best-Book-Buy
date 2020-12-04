b06cd57dddc0b2fc6f6e3326a72622f690955319
Nathan Fishel <n_fishel-v@dcu.org>
2020-12-04 18:27:02 -0500

    buy it button

---
+++ checkout.php
@@ -0,0 +1,10 @@
+<?
+$db = pg_connect("host=ec2-3-218-75-21.compute-1.amazonaws.com dbname=d8p0qs8v3fbf9m user=gymsvpkhkckshh password=68db7ff943798b07abc442d46449c9d2f4bfcd38be0f79023a630bf67b3b3a8a");
+$update = "update Order_book set order_number = 5 where order_number = 1;";
+echo $update;
+pg_query($db, $update);
+pg_close($db);
+//header("Location: https://best-book-buy.herokuapp.com/confirm_order.php?checkout_submit=Proceed+to+Checkout"); 
+//exit;
+
+?>
