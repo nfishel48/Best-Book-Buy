fd729480a68ae764044e96a8fdbc2679de8d584c
Nick <nick.gilliam2@gmail.com>
2020-12-04 17:57:10 -0500

    added redirect if not logged in

--- confirm_order.php
+++ confirm_order.php
@@ -7,6 +7,13 @@
 <body>
 <?
 	$db = pg_connect("host=ec2-3-218-75-21.compute-1.amazonaws.com dbname=d8p0qs8v3fbf9m user=gymsvpkhkckshh password=68db7ff943798b07abc442d46449c9d2f4bfcd38be0f79023a630bf67b3b3a8a");
+	
+	$is_logged_in = pg_query($db, 'select count(*) from customer where logged_in = true;');
+	
+	if (pg_num_rows($is_logged_in) == 0){
+		echo("<script type = \"text/javascript\">window.location = \"user_login.php\";</script>");
+	}
+	
 	$order = pg_query($db, 'select * from Order_t where placed = false;');
 	$row = pg_fetch_row($order);
 	$orderNumber = $row[0];
