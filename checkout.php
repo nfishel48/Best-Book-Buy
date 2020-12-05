e882918e6866bc266aa91142a7a9dbec6358ddb0
Nathan Fishel <n_fishel-v@dcu.org>
2020-12-04 22:11:17 -0500

    fixed checkout

--- checkout.php
+++ checkout.php
@@ -2,8 +2,8 @@
 $db = pg_connect("host=ec2-3-218-75-21.compute-1.amazonaws.com dbname=d8p0qs8v3fbf9m user=gymsvpkhkckshh password=68db7ff943798b07abc442d46449c9d2f4bfcd38be0f79023a630bf67b3b3a8a");
 $update = "delete from order_book where order_number = 1;";
 pg_query($db, $update);
-$month;
-switch(date("m")){
+$month =date("m"); 
+switch($month){
     case 1:
         $month = "January";
         break;
