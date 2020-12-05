cf9c9bd4c6a68d946d01a5c94ab7635273ccc851
Nathan Fishel <n_fishel-v@dcu.org>
2020-12-04 21:17:55 -0500

    merge conflict

--- admin_tasks.php
+++ admin_tasks.php
@@ -30,7 +30,7 @@ $db = pg_connect("host=ec2-3-218-75-21.compute-1.amazonaws.com dbname=d8p0qs8v3f
 			rsort($books);
 			foreach($books as $x => $x_value) {
 				$row = pg_fetch_row($x_value);
-				echo "<p>".$x." ".$row[0]."</p>";
+				echo "<p>".$row[1]." ".$row[0]."</p>";
 			}
 
 			echo '</td>';
