e78d83228cfe43b7a6243acaa14cfa9e5b700d35
Nathan Fishel <n_fishel-v@dcu.org>
2020-12-04 22:54:33 -0500

    working on checkout

--- checkout2.php
+++ checkout2.php
@@ -43,7 +43,7 @@ switch($month){
 $sale = "Select Ammount from Sales where Month ='".$month."';";
 $total = pg_query($db, $sale);
 $total = $total +$_POST['submit'];
-echo $_POST['submiit'];
+echo $_POST['submit'];
 echo $total;
 $sale2 = "Update Sales Set Ammount =".$total.";";
 pg_query($db, $sale2); 
