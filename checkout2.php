ecb1e58aa402db229e46c9e48f107b2ed1cffffe
Nathan Fishel <n_fishel-v@dcu.org>
2020-12-04 22:27:19 -0500

    fixing checkout

--- checkout2.php
+++ checkout2.php
@@ -1,11 +1,11 @@
 <?
 $db = pg_connect("host=ec2-3-218-75-21.compute-1.amazonaws.com dbname=d8p0qs8v3fbf9m user=gymsvpkhkckshh password=68db7ff943798b07abc442d46449c9d2f4bfcd38be0f79023a630bf67b3b3a8a");
 $update = "delete from order_book where order_number = 1;";
-echo $update;
+
 pg_query($db, $update);
 $month = date("m");
-echo $_POST;
-echo gettype($_POST);
+echo $_POST[0];
+echo gettype($_POST[0]);
 switch($month){
     case 1:
         $month = "January";
