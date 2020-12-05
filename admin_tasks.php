2b88e1d5889b2fed6af15494c3c7cc85c44cde1e
Nathan Fishel <n_fishel-v@dcu.org>
2020-12-04 20:33:40 -0500

    added colon

--- admin_tasks.php
+++ admin_tasks.php
@@ -20,9 +20,9 @@ $db = pg_connect("host=ec2-3-218-75-21.compute-1.amazonaws.com dbname=d8p0qs8v3f
 			echo '<td>';
 			$categories = pg_query($db, "Select DISTINCT category from book;");
 			$books = array();
-			echo $categories;
-			foreach($categories as $category){
-				$books[$category] = pg_query($db, "Select COUNT(*) from book where category ='".$category."';");
+			foreach($categories as $value){
+				echo $categories;
+				$books[$value] = pg_query($db, "Select COUNT(*) from book where category ='".$value."';");
 			}
 			rsort($books);
 			foreach($books as $x => $x_value) {
